fba65ae8562ce465068d05eebc8879bee1891cc0
victorokonkwo67@gmail.com <victorokonkwo67@gmail.com>
2022-05-24 21:35:46 +0100

    feat(nav/, index.css): add font
    add font to index.css,
    add toggle feature for mobile navigation items.

--- src/components/nav/nav-mobile/nav.mobile.component.tsx
+++ src/components/nav/nav-mobile/nav.mobile.component.tsx
@@ -1,13 +1,36 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState, useRef } from 'react';
 
-import { MobileNavBar, Navigation, Overlay } from './nav.mobile.styles';
+import {
+  CloseButtonContainer,
+  MobileNavBar,
+  Navigation,
+  NavigationItems,
+  NavigationItemsOptions,
+  Overlay,
+} from './nav.mobile.styles';
 
 import { ReactComponent as HamBurger } from '../../../assets/icon-menu.svg';
 import { ReactComponent as CloseIcon } from '../../../assets/icon-close-menu.svg';
 import { ReactComponent as Logo } from '../../../assets/logo.svg';
+import { ReactComponent as ArrowUp } from '../../../assets/icon-arrow-down.svg';
+import { ReactComponent as ArrowDown } from '../../../assets/icon-arrow-up.svg';
 
 const MobileNav: FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const [isOptionOpen, setOptionOpen] = useState<boolean>(false);
+
+  const ref = useRef<HTMLSpanElement>(null);
+
+  const closeNav = (e: Event): void => {
+    if (ref.current === e.target) setIsOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', closeNav);
+
+    return () => document.removeEventListener('click', closeNav);
+  });
 
   return (
     <MobileNavBar>
@@ -16,17 +39,40 @@ const MobileNav: FC = () => {
         attributeName="hamburger menu"
         title="Hamburger"
         onClick={() => setIsOpen((prev) => !prev)}
+        style={{ cursor: 'pointer' }}
       />
       {isOpen && (
         <>
-          <Overlay />
+          <Overlay ref={ref} />
           <Navigation>
-            <CloseIcon onClick={() => setIsOpen((prev) => !prev)} />
-            <ul>
-              <li>features</li>
-              <li>careers</li>
-              <li>about</li>
-            </ul>
+            <CloseButtonContainer>
+              <CloseIcon onClick={() => setIsOpen((prev) => !prev)} />
+            </CloseButtonContainer>
+            <NavigationItems>
+              <NavigationItemsOptions
+                role="navigation"
+                onClick={() => setOptionOpen((next) => !next)}
+              >
+                <p>features {isOptionOpen ? <ArrowDown /> : <ArrowUp />}</p>
+                {isOptionOpen && (
+                  <ul>
+                    <li>todo list</li>
+                    <li>calendar</li>
+                    <li>reminder</li>
+                    <li>planning</li>
+                  </ul>
+                )}
+              </NavigationItemsOptions>
+              <NavigationItemsOptions role="navigation">
+                <p> company {isOptionOpen ? <ArrowDown /> : <ArrowUp />}</p>
+              </NavigationItemsOptions>
+              <NavigationItemsOptions role="navigation">
+                careers
+              </NavigationItemsOptions>
+              <NavigationItemsOptions role="navigation">
+                about
+              </NavigationItemsOptions>
+            </NavigationItems>
           </Navigation>
         </>
       )}
--- src/components/nav/nav-mobile/nav.mobile.styles.ts
+++ src/components/nav/nav-mobile/nav.mobile.styles.ts
@@ -1,23 +1,30 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const slideInFromRight = keyframes`
+  from { width: 0%; }
+  to { width: 60%;  }
+`;
 
 export const MobileNavBar = styled.header`
   grid-row: header;
   grid-column: full-bleed;
   display: flex;
   justify-content: space-between;
-  padding: 15px;
   position: relative;
+  padding: 15px;
 `;
 
 export const Navigation = styled.nav`
   height: 100vh;
-  padding: calc(32rem / 26) calc(48rem / 16);
+  padding: calc(22rem / 16);
   position: absolute;
   top: 0;
   right: 0;
   background-color: hsl(0, 0%, 98%);
   z-index: 2;
-  width: min-content;
+  width: 60%;
+  animation: ${slideInFromRight} 0.4s cubic-bezier(0, 1.61, 0.81, 0.83);
+  box-sizing: content-box;
 `;
 
 export const Overlay = styled.span`
@@ -28,4 +35,36 @@ export const Overlay = styled.span`
   z-index: 1;
   height: 100vh;
   width: 100vw;
+`;
+
+export const NavigationItems = styled.ul`
+  text-transform: capitalize;
+  font-size: calc(16.5rem / 16);
+  font-weight: 700;
+  color: hsl(0, 0%, 41%);
+
+  & > :not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+export const NavigationItemsOptions = styled.li`
+  list-style: none;
+  cursor: pointer;
+
+  & > p {
+    display: flex;
+    align-items: center;
+
+    & > svg {
+      margin-left: calc(16rem / 16);
+    }
+  }
+`;
+
+export const CloseButtonContainer = styled.span`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: calc(32rem / 16);
+  cursor: pointer;
 `;
